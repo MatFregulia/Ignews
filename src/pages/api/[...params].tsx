1bbba92c44c4a37a130408d4a42b5647d38a7b81
Matheus Fregulia <matheus.gviva@gmail.com>
2022-01-10 09:22:00 -0300

    Parametrização das rotas

--- src/pages/api/[...params].tsx
+++ src/pages/api/[...params].tsx
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (request: NextApiRequest, response: NextApiResponse) => {
+    console.log(request.query)
+
     const users = [
         { id: 1, name: 'Matheus' },
         { id: 2, name: 'Dani' },
